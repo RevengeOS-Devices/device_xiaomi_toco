ea58d54c1d1e08ea95a4f63a9f57793072479864
alibei <sebastiankotzias83@gmail.com>
2020-12-02 18:55:04 +0100

    tucana: add fod

---
+++ fod/FingerprintInscreen.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "FingerprintInscreenService"
+
+#include "FingerprintInscreen.h"
+
+#include <android-base/logging.h>
+#include <cmath>
+#include <fstream>
+
+#define COMMAND_NIT 10
+#define PARAM_NIT_630_FOD 1
+#define PARAM_NIT_NONE 0
+
+#define Touch_Fod_Enable 10
+#define Touch_Aod_Enable 11
+
+#define FOD_SENSOR_X 445
+#define FOD_SENSOR_Y 1715
+#define FOD_SENSOR_SIZE 190
+
+namespace vendor {
+namespace lineage {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+
+    file >> result;
+    return file.fail() ? def : result;
+}
+
+FingerprintInscreen::FingerprintInscreen() {
+    TouchFeatureService = ITouchFeature::getService();
+    xiaomiDisplayFeatureService = IDisplayFeature::getService();
+    xiaomiFingerprintService = IXiaomiFingerprint::getService();
+}
+
+Return<int32_t> FingerprintInscreen::getPositionX() {
+    return FOD_SENSOR_X;
+}
+
+Return<int32_t> FingerprintInscreen::getPositionY() {
+    return FOD_SENSOR_Y;
+}
+
+Return<int32_t> FingerprintInscreen::getSize() {
+    return FOD_SENSOR_SIZE;
+}
+
+Return<void> FingerprintInscreen::onStartEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onFinishEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onPress() {
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_630_FOD);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onRelease() {
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onShowFODView() {
+    TouchFeatureService->setTouchMode(Touch_Fod_Enable, 1);
+    xiaomiDisplayFeatureService->setFeature(0, 17, 1, 255);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onHideFODView() {
+    TouchFeatureService->resetTouchMode(Touch_Fod_Enable);
+    xiaomiDisplayFeatureService->setFeature(0, 17, 0, 255);
+    return Void();
+}
+
+Return<bool> FingerprintInscreen::handleAcquired(int32_t acquiredInfo, int32_t vendorCode) {
+    LOG(ERROR) << "acquiredInfo: " << acquiredInfo << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<bool> FingerprintInscreen::handleError(int32_t error, int32_t vendorCode) {
+    LOG(ERROR) << "error: " << error << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
+    return Void();
+}
+
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
+    return 0;
+}
+
+Return<bool> FingerprintInscreen::shouldBoostBrightness() {
+    return false;
+}
+
+Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>&) {
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace lineage
+}  // namespace vendor
