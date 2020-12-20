18231141666788c88f5c943a90211bcd79f288b2
alibei <sebastiankotzias83@gmail.com>
2020-12-20 20:24:27 +0100

    tucana: fix wireless display

--- device.mk
+++ device.mk
@@ -212,5 +212,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
+
 #PRODUCT_BOOT_JARS += \
     WfdCommon
