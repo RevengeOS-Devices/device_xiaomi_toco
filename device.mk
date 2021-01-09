1e153ce2da3264c7538de92b0cdf786a684c0e90
xyyx <xyyx@mail.ru>
2021-01-09 20:31:27 +0300

    toco: Add lineage recovery support

--- device.mk
+++ device.mk
@@ -122,7 +122,8 @@ PRODUCT_BOOT_JARS += \
 # Init
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc \
-    init.qcom.rc
+    init.qcom.rc \
+    init.recovery.qcom.rc
 
 # Input
 PRODUCT_COPY_FILES += \
