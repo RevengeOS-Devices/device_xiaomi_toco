c43702ffe669ff37b587888a92a5d6ee55f85a8f
Arian <arian.kulmer@web.de>
2021-06-21 15:15:37 +0200

    Revert "toco: fod: Add support for Xiaomi's touchfeature"
    This reverts commit c2b6d1096950af1f5fda26ab65603a88f5989411.
    
    Change-Id: I3b2f5914f2f44c7c4d45ab4570f8af8aab0ba766

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,6 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Sepolicy
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # USB
